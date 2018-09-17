ccd5e8291562e71e1ca5b537de4fac59b9d808a3
ken <mail@keshuang.net>
2018-09-18 03:36:18 +0800

    使用ASP.NET Core MVC框架构建Web应用

---
+++ chapter-02/Ken.Tutorial/Ken.Tutorial.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ken.Tutorial.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World ！ -ken.io");
+        }
+
+        public IActionResult Time()
+        {
+            //将当前服务器时间放入ViewBag中
+            ViewBag.ServerTime = DateTime.Now;
+            return View("Time");
+        }
+    }
+}
