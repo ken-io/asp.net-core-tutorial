c4ee7ba6b228679d5a7c4a5120dd5ef8ec072957
ken <m@ken.io>
2018-12-20 21:31:34 +0800

    ASP.NET Core MVC 分部视图入门

--- chapter-07/Ken.Tutorial/Ken.Tutorial.Web/Startup.cs
+++ chapter-07/Ken.Tutorial/Ken.Tutorial.Web/Startup.cs
@@ -11,7 +11,7 @@ namespace Ken.Tutorial.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container. 
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
